0f79fcf9007c3c997cd0d9dac20951087cf5c1be
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2021-01-19 09:12:29 +0300

    Update script

--- check.py
+++ check.py
@@ -15,14 +15,11 @@ def send_message(bot, text):
 user_chat_id = os.getenv('USER_CHAT_ID')
 token = os.getenv('BOT_TOKEN')
 bot = Bot(token=token)
-bot.send_message(chat_id=user_chat_id, text='Bot is working')
+chromedriver = 'chromedriver'
 while True:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    browser = webdriver.Chrome(
-        executable_path=os.environ.get("CHROMEDRIVER_PATH"),
-        chrome_options=chrome_options)
+    browser = webdriver.Chrome(chromedriver, options=chrome_options)
     browser.get(
         'https://showroom.hyundai.ru/')
     soup = BeautifulSoup(browser.page_source, 'lxml')
